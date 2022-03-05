4ff21b2694fd6e534379ba61e7554d16a11b1c34
sounakbh <sounakbh@gmail.com>
2022-03-05 09:10:50 -0500

    Added labs

--- src/App.js
+++ src/App.js
@@ -1,7 +1,30 @@
 import "./App.css";
+// import './vendors/bootstrap/css/bootstrap.min.css';
+import "./vendors/bootstrap/css/bootstrap.min.css";
+import "./vendors/bootstrap/bootstrap.min.css";
+// import "./vendors/fontawesome/css/all.min.css";
+import "./vendors/fontawesome/css/font-awesome.min.css";
+import HelloWorld from "./components//HelloWorld";
+import Labs from "./components/Labs/index.js";
+import Tuiter from "./components/Tuiter/index.js";
+import { BrowserRouter, Route } from "react-router-dom";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <BrowserRouter>
+      <div className="container">
+        <Route path="/hello" exact={true}>
+          <HelloWorld />
+        </Route>
+        <Route path="/labs" path={["/", "/labs"]} exact={true}>
+          <Labs />
+        </Route>
+        <Route path="/tuiter" exact={true}>
+          <Tuiter />
+        </Route>
+      </div>
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ src/components/Labs/index.js
@@ -0,0 +1,22 @@
+import React from "react";
+import Classes from "./Classes";
+import Styles from "./Styles";
+import ConditionalOutput from "./ConditionalOutput";
+import TodoItem from "./Todo/TodoItem";
+import TodoList from "./Todo/TodoList";
+import { Link } from "react-router-dom";
+
+const Labs = () => {
+  return (
+    <>
+      <h1>Labs</h1>
+      <Link to="/hello">Hello</Link> |<Link to="/tuiter">Tuiter</Link>
+      <ConditionalOutput />
+      <Styles />
+      <Classes />
+      <TodoList />
+    </>
+  );
+};
+
+export default Labs;
